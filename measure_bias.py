e982a7455184518c9ee9580cba53a4fd3a0bbf67
FannyDcl <ducelfanny@gmail.com>
2024-06-13 10:58:58 +0200

    better print and add symptoms list and GS examples

--- measure_bias.py
+++ measure_bias.py
@@ -2,6 +2,8 @@
 TO RUN with command such as : python measure_bias.py FR neutral
 If ran for the gendered setting, returns both the Gender Gap and the Gender Shift.
 If ran for neutral, returns the Gender Gap."""
+import json
+
 import pandas as pd
 from tabulate import tabulate
 import sys
@@ -11,20 +13,20 @@ import sys
 import numpy as np
 
 # TODO : ajouter filtre pour maladies stéréotypées
-class MyParser(argparse.ArgumentParser):
-    def error(self, message):
-        sys.stderr.write('error: %s\n' % message)
-        self.print_help()
-        sys.exit(2)
+# class MyParser(argparse.ArgumentParser):
+#     def error(self, message):
+#         sys.stderr.write('error: %s\n' % message)
+#         self.print_help()
+#         sys.exit(2)
 
-parser = MyParser()
-# parser.add_argument('language', choices=['FR', 'IT'])
-parser.add_argument('experiment_type', choices=['neutral', 'gendered'])
-
-args = parser.parse_args()
-
-#language = sys.argv[1]
-type_expe = sys.argv[1]
+# parser = MyParser()
+# # parser.add_argument('language', choices=['FR', 'IT'])
+# parser.add_argument('experiment_type', choices=['neutral', 'gendered'])
+#
+# args = parser.parse_args()
+#
+# #language = sys.argv[1]
+# type_expe = sys.argv[1]
 #language = "FR"
 #type_expe = "neutral"
 dic_df = {}
@@ -88,7 +90,7 @@ def gender_gap(topics, filter, data_genre=data_genre):
 
 def gender_shift(df):
     """Renvoie la probabilité que le prompt ne soit pas respecté (= nb de fois où le texte est généré dans le genre opposé ou ambigu)"""
-    # df.replace({"['Prompt_masc']":"Masculine", "['Prompt_fém']":"Feminine"}, inplace=True)
+    df.replace({"masculin":"Masculine", "féminin":"Feminine"}, inplace=True)
 
     df['gender_shift'] = np.where((df['sex_prompt'] != df['Identified_gender']) & (df['sex_prompt'] == "Undetermined") & (
                 df['Identified_gender'] != "Neutral") & (df['Identified_gender'] != "incomplet/pas de P1"), 1, 0)
@@ -98,22 +100,41 @@ def gender_shift(df):
     df['gender_shift'] = np.where((df['sex_prompt'] != df['Identified_gender']) & (df['Identified_gender'] != "Neutral") & (
                 df['Identified_gender'] != "incomplet/pas de P1"), 1, 0)
 
+    # print IDs of files that have a positive gender shift
+    positive_gf = df.loc[df["gender_shift"] == 1, "fichier_ref"].to_list()
+    print("List of files with positive GS :", [(el, id_symptoms(el)) for el in positive_gf])
     # df.to_csv("gender_shift_noneutral.csv")
     return sum(df['gender_shift']) / len(df['gender_shift'])
+
+def id_symptoms(id) :
+    """For a given document reference (id), returns the DISO elements of the json file, e.g. the symptoms associated with
+    the clinical cases. To use in order to link IDs of the highest Gender Gaps to the symptoms."""
+    with open("generated_data/raw_json/vigogne-2-7b_5-consts.json", "r") as f:
+        data = json.load(f)
+    for dic in data :
+        if dic["fichier"] == id :
+            symptoms = [el[0] for el in dic["constraints"] if el[-1] == "DISO"]
+            return symptoms
 
 
 all_sorted_gap, all_masc_gap, all_fem_gap = gender_gap(topics,"all")
 mean_gap_total = sum([el[1] for el in all_sorted_gap])/len(all_sorted_gap)
-print("The global Gender Gap in the", type_expe ,"setting is of", mean_gap_total)
-print("The 10 professional fields with the highest Gender Gaps are", all_sorted_gap[:10])
-print("The 10 professional fields with the lowest Gender Gaps are", all_sorted_gap[-10:])
+print("====== ON ALL PROMPTS (GENDERED + NEUTRAL)  ======")
+print("The global Gender Gap is of", mean_gap_total)
+print("The 10 diseases (ref doc) with the highest Gender Gaps are", all_sorted_gap[:10])
+print("The 10 diseases (ref doc) with the lowest Gender Gaps are", all_sorted_gap[-10:])
 
 all_sorted_gap, all_masc_gap, all_fem_gap = gender_gap(topics,"undetermined")
 mean_gap_total = sum([el[1] for el in all_sorted_gap])/len(all_sorted_gap)
-print("The global Gender Gap ONLY ON NEUTRAL in the", type_expe ,"setting is of", mean_gap_total)
-print("The 10 professional fields with the highest Gender Gaps are", all_sorted_gap[:10])
-print("The 10 professional fields with the lowest Gender Gaps are", all_sorted_gap[-10:])
+print("\n====== ONLY ON NEUTRAL  ======")
+print("The global Gender Gap is of", mean_gap_total)
+print("\n\tTop 10 highest Gender Gaps (id, symptoms list)")
+for id in all_sorted_gap[:10] :
+    print(id, id_symptoms(id[0]))
+print("\n\tTop 10 lowest Gender Gaps (id, symptoms list)")
+for id in all_sorted_gap[-10:] :
+    print(id, id_symptoms(id[0]))
 
-if type_expe == "gendered":
-    print("The global Gender Shift in the", type_expe,"setting is of", gender_shift(data_genre))
+print("\n")
+print("\nThe global Gender Shift is of", gender_shift(data_genre))
 
