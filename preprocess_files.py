1a45baf67247067a08966f3cd199e5c13017b336
FannyDcl <ducelfanny@gmail.com>
2024-08-09 10:08:54 +0200

    files and script for repetitiveness

--- preprocess_files.py
+++ preprocess_files.py
@@ -24,13 +24,16 @@ def json_to_df(chemin_json):
         #sex = dic["input"].split(";")[0].split(":")[-1].strip() if "Sexe" in dic["input"].split(";")[0] else "Undetermined"
         #age = dic["input"].split(";")[1].split(":")[-1].strip() if "Age" in dic["input"].split(";")[1] else dic["input"].split(";")[0].split(":")[-1].strip()
         for i, generation in enumerate(dic["candidats"]):
-            texte = {"fichier_ref": dic["fichier"], "pathologie":dic["pathologie"], "generation": generation, "input":dic["input"], "sex_prompt":dic["sexe"], "age_prompt":dic["age"], "nb_contraintes":dic["nb_contraintes"], "respect_contraintes":dic["respected_rate"][i]}
+            texte = {"fichier_ref": dic["fichier"], "pathologie":dic["pathologie"], "generation": generation, "input":dic["input"], "sex_prompt":dic["sexe"], "age_prompt":dic["age"], "nb_contraintes":dic["nb_contraintes"], "respect_contraintes":dic["respected_rate"][i],
+                     "has_reps":dic["has_reps"][i], "scores_reps":dic["scores_reps"][i], "debuts_boucles":dic["debuts_boucles"], "prop_avant_boucle":dic["prop_avant_boucle"][i]}
             contenu_df.append(texte)
     df = pd.DataFrame(contenu_df)
-    df.to_csv(f"generated_data/generations_{modele}.csv")
+    #df.to_csv(f"generated_data/generations_{modele}.csv")
+    df.to_csv(f"generations_scores_repetitions/{modele}_repetitions.csv")
 
 #vigogne = "generated_data/raw_json/vigogne-2-7b_10-consts_infos.json"
 #json_to_df(vigogne)
 
-for file in glob.glob(f"generated_data/raw_json/data_biais/*"):
+#for file in glob.glob(f"generated_data/raw_json/data_biais/*"):
+for file in glob.glob(f"generations_scores_repetitions/*"):
     json_to_df(file)
---
+++ repetition.py
@@ -0,0 +1,32 @@
+"""Use repetitiveness scores to compute averages and correlations between scores_reps (and/or has_reps, prop_avant_boucle, debuts_boucles)
+and model/pathology/generated gender"""
+
+import pandas as pd
+import glob
+
+for file in glob.glob("generations_scores_repetitions/*.csv"):
+    model = file.split("/")[-1].split("_")[0]
+    df = pd.read_csv(file)
+    df.fillna("N/A", inplace=True)
+
+    df_annotated = pd.read_csv(f"annotated_data/generations_{model}_10-consts_infos_gender_trf.csv")
+    df["Identified_gender"] = df_annotated["Identified_gender"]
+    print(file)
+    print(df["scores_reps"].mean(), df["scores_reps"].std())
+    print(df.groupby("pathologie")["scores_reps"].mean())
+
+    patho = {el:i  for i,el in enumerate(list(set(df['pathologie']))) }
+    df['pathologie'] = df['pathologie'].replace(patho)
+    print("Corrélation scores_reps x patho",df["scores_reps"].corr(df["pathologie"]))
+
+    print(df.groupby("sex_prompt")["scores_reps"].mean())
+    sex_prompt = {el: i for i, el in enumerate(list(set(df['sex_prompt'])))}
+    df['sex_prompt'] = df['sex_prompt'].replace(sex_prompt)
+    print("Corrélation scores_reps x sex_prompt", df["sex_prompt"].corr(df["scores_reps"]))
+
+    print(df.groupby("Identified_gender")["scores_reps"].mean())
+    gender = {el: i for i, el in enumerate(list(set(df['Identified_gender'])))}
+    df['Identified_gender'] = df['Identified_gender'].replace(gender)
+    print("Corrélation scores_reps x Identified_gender", df["Identified_gender"].corr(df["scores_reps"]))
+    print("\n*********************")
+    #todo: groupby("Identified_gender") mais colonne à ajouter
